08f1e28f514154497d43f3a180214bcdecb76cce
Hanibal Girmay <hanibalgirmay@gmail.com>
2023-09-11 15:26:34 -0700

    remove unwanted file

--- utils/types.ts
+++
@@ -1,9 +0,0 @@
-export interface APIResponse {
-  slack_name: string;
-  current_day: string;
-  utc_time: Date;
-  track: string;
-  github_file_url: string;
-  github_repo_url: string;
-  status_code: number;
-}
